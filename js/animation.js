525e3c398b16256f8e1895422aa0c27ec32f10f8
Lars Johannessen <lars.johannessen@sbsdiscovery.no>
2016-02-05 15:36:31 +0100

    Finished frontend

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,8 @@
+/*
 
 (function($) {
  
-    var $formmsg = $('.bg #formmsg'),
+    var $formmsg = $('.container'),
     	$feedbackmsg = $('.bg #feedbackmsg');
     	tlProject;
     	
@@ -15,3 +16,44 @@
     	tlProject.fromTo($heading, 0.4, {autoAlpha: 0, yPercent = '-200' },{ yPercent ='0', ease:Power4.easeInOut });
  
 })(jQuery);
+*/
+
+$(document).ready(function(){
+   
+   alert("hello");
+
+/*
+    $( '#feedbackmsg' ).hide();
+    TweenMax.set('.bg',{opacity:0});
+    TweenMax.set('h1',{opacity:0, y: 0 });
+    TweenMax.set('#formmsg',{opacity:0, y: 20});
+   
+    
+
+   function backgroundIn(){
+    	TweenMax.to('.bg',.7,{opacity:1, onComplete: animateIn });
+   }
+  
+   function animateIn(){
+    	TweenMax.staggerTo('.anim',.5,{opacity:1, y:-20, ease:Power4.easeInOut,},.2);
+   }
+*/
+   
+*/
+   /*
+function loadnewpage(){
+    	TweenMax.staggerTo('.anim',.5,{opacity:1, y:-20, ease:Power4.easeInOut},.2);
+   }
+*/
+     
+   /* MOVE OUT */
+/*
+   $('.btn').click(function(){
+   		TweenMax.staggerTo('.anim' ,1,{opacity:0, y:-20, ease:Expo.easeOut},.1);
+   });
+
+ 
+    backgroundIn();
+   */
+    		
+});
